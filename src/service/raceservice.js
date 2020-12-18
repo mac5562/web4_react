3c8d5bdec4f44dfa2c8290fcb71ab47b1e513352
mac5562 <55428222+mac5562@users.noreply.github.com>
2020-12-18 21:41:24 +0100

    raceservice.js feltöltése
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

---
+++ src/service/raceservice.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/api/Race/';
+
+
+class RaceService{
+
+    create( kor,nem,datum,helyszin,tav,max_resztvevo,resztvevok,dobogosok){
+        return axios.post(API_URL+'create',{
+            kor,
+            nem,  
+            datum,
+            helyszin,
+            tav,
+            max_resztvevo,
+            resztvevok,
+            dobogosok, 
+        });
+    }
+   
+    getRaces(){
+        return axios.get(API_URL+'get',{ headers: authHeader() });
+    }
+   getRace(id){
+    return axios.get(API_URL+'get'+id,{ headers: authHeader() });
+   }
+deleteRace(){
+    return axios.delete(API_URL+'get'+id,{ headers: authHeader() });
+}
+
+updateRace(id,kor,nem,datum,helyszin,tav,max_resztvevo,resztvevok,dobogosok){
+    return axios.put(API_URL+'get'+id,{
+        kor,
+        nem,  
+        datum,
+        helyszin,
+        tav,
+        max_resztvevo,
+        resztvevok,
+        dobogosok, 
+    });
+}
+
+}
+
+export default new RaceService();
