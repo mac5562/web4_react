2541941d528263abbf647955063cc280d618521f
mac5562 <55428222+mac5562@users.noreply.github.com>
2020-12-18 23:47:48 +0100

    type.js létrehozása
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

---
+++ src/action/type.js
@@ -0,0 +1,16 @@
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const READ_FAIL="READ_FAIL";
+export const READ_SUCCESS="READ_SUCCESS";
+export const DELETE_FAIL="DELETE_FAIL";
+export const DELETE_SUCCESS="DELETE_SUCCESS";
+export const UPDATE_FAIL="UPDATE_FAIL";
+export const UPDATE_SUCCESS="UPDATE_SUCCESS";
+export const CREATE_FAIL="CREATE_FAIL";
+export const CREATE_SUCCESS="CREATE_SUCCESS";
+
+export const SET_MESSAGE = "SET_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
